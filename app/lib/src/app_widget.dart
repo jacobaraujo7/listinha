646e11f1fb29e85b5ab63dec253612ef5a94d1c4
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-10 20:55:29 -0300

    feat: add app_widget

--- app/lib/src/app_widget.dart
+++ app/lib/src/app_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:listinha/src/home/home_page.dart';
 
-import '../main.dart';
 import 'shared/themes/themes.dart';
 
 class AppWidget extends StatelessWidget {
@@ -14,7 +14,7 @@ class AppWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       darkTheme: darkTheme,
-      home: const MyHomePage(title: 'Listinha'),
+      home: const HomePage(),
     );
   }
 }
