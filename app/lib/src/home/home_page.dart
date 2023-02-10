6895905cd0214cd5db0c160d4f256ab1981723fa
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-10 20:55:50 -0300

    feat: add home_page

---
+++ app/lib/src/home/home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:listinha/src/home/widgets/custom_drawer.dart';
+import 'package:listinha/src/shared/widgets/user_image_button.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: const CustomDrawer(),
+      appBar: AppBar(
+        title: const Text('Listinha'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: UserImageButton(),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: SegmentedButton(
+                segments: const [
+                  ButtonSegment(
+                    value: 0,
+                    label: Text('Todos'),
+                  ),
+                  ButtonSegment(
+                    value: 1,
+                    label: Text('Pendentes'),
+                  ),
+                  ButtonSegment(
+                    value: 2,
+                    label: Text('Concluídas'),
+                  ),
+                  ButtonSegment(
+                    value: 3,
+                    label: Text('Desativadas'),
+                  ),
+                ],
+                selected: const {1},
+                onSelectionChanged: (values) {},
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.edit),
+        label: const Text('Nova Lista'),
+        onPressed: () {},
+      ),
+    );
+  }
+}
