a7c2866e764ed2c53105298eb4e3ad719da23afc
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-13 14:10:56 -0300

    feat: add route navigator

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -57,7 +57,9 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: FloatingActionButton.extended(
         icon: const Icon(Icons.edit),
         label: const Text('Nova Lista'),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).pushNamed('./edit');
+        },
       ),
     );
   }
