6c2cc4fda0fc58e0c9a79c05470c0bc704eb3ab2
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-13 14:12:17 -0300

    feat: add config widget

---
+++ app/lib/src/configuration/configuration_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ConfigurationPage extends StatefulWidget {
+  const ConfigurationPage({Key? key}) : super(key: key);
+
+  @override
+  State<ConfigurationPage> createState() => _ConfigurationPageState();
+}
+
+class _ConfigurationPageState extends State<ConfigurationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Configuration'),
+      ),
+      body: Container(),
+    );
+  }
+}
