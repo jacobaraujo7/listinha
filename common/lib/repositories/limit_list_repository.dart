7e95694ccc75237766a9686f5c2eea7022613664
Jacob Moura <jacobaraujo7@gmail.com>
2023-03-13 10:33:45 -0300

    uploa

---
+++ common/lib/repositories/limit_list_repository.dart
@@ -0,0 +1,3 @@
+abstract class LimitListRepository {
+  bool isForbbider();
+}
---
+++ common/test/usecases/create_board_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('create board ...', () {
+    // TODO: Implement test
+  });
+}
